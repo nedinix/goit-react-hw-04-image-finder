5d5c49a9c2bba7b30c0438ab5410d162046bc3d1
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-06-15 15:45:49 +0300

    Update api-service.js

--- src/api-service/api-service.js
+++ src/api-service/api-service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '35839995-5c49d25fb3199a064f9ba676b';
 
-axios.defaults.baseURL = `https://pixabay.com/apji/`;
+axios.defaults.baseURL = `https://pixabay.com/api/`;
 axios.defaults.params = {
   key: API_KEY,
   image_type: 'photo',
